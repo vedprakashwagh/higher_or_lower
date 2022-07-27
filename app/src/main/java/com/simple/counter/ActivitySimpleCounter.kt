0147249e8e4092ec1cdc2deebf5ac975fe1d0c33
Vedprakash Wagh <tigerwed@gmail.com>
2022-07-27 15:41:27 +0530

    Day 7

--- app/src/main/java/com/simple/counter/ActivitySimpleCounter.kt
+++ app/src/main/java/com/simple/counter/ActivitySimpleCounter.kt
@@ -10,15 +10,36 @@ import androidx.appcompat.app.AppCompatActivity
 class ActivitySimpleCounter: AppCompatActivity() {
 
     var currentCounterNumber: Int = 0
+
+    val INCREMENTED_NUMBER = "INCREMENTED_NUMBER"
+    val SHARED_PREFERENCE_FILE = "COUNTER"
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_simple_counter)
+        currentCounterNumber = getCounterNumber()
+
         var textViewCounterNumber: TextView = findViewById(R.id.counterNumber)
+        textViewCounterNumber.text = currentCounterNumber.toString()
         var buttonIncrement: Button = findViewById(R.id.buttonIncrement)
         buttonIncrement.setOnClickListener {
             currentCounterNumber += 1
+            saveCounterNumber(currentCounterNumber)
             textViewCounterNumber.text = currentCounterNumber.toString()
         }
     }
+
+    fun saveCounterNumber(number: Int) {
+        val sharedPreference = getSharedPreferences(SHARED_PREFERENCE_FILE, MODE_PRIVATE)
+        sharedPreference.edit().putInt(INCREMENTED_NUMBER, number).commit()
+    }
+
+    private fun getCounterNumber() : Int {
+        val sharedPreference = getSharedPreferences(SHARED_PREFERENCE_FILE, MODE_PRIVATE)
+        return sharedPreference.getInt(INCREMENTED_NUMBER, 0)
+    }
+
 
 }
